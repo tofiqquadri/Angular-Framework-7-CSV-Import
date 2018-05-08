d557864cab18d51fd86111aa4ffca92744cdd885
Tofiq Quadri <tofiq.quadri@itoptix.com>
2018-05-08 12:37:20 +0530

    #Angular 5 CSV Import Implementation

---
+++ src/app/app.component.ts
@@ -0,0 +1,109 @@
+import { Component } from '@angular/core';
+import { ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+
+export class AppComponent {
+
+  title = 'app';
+  public csvRecords: any[] = [];
+
+  @ViewChild('fileImportInput') fileImportInput: any;
+
+
+  fileChangeListener($event: any): void {
+
+    var text = [];
+    var files = $event.srcElement.files;
+
+    if (this.isCSVFile(files[0])) {
+
+      var input = $event.target;
+      var reader = new FileReader();
+      reader.readAsText(input.files[0]);
+
+      reader.onload = (data) => {
+        let csvData = reader.result;
+        let csvRecordsArray = csvData.split(/\r\n|\n/);
+
+        let headersRow = this.getHeaderArray(csvRecordsArray);
+
+        this.csvRecords = this.getDataRecordsArrayFromCSVFile(csvRecordsArray, headersRow.length);
+      }
+
+      reader.onerror = function() {
+        alert('Unable to read ' + input.files[0]);
+      };
+
+    } else {
+      alert("Please import valid .csv file.");
+      this.fileReset();
+    }
+  }
+
+  getDataRecordsArrayFromCSVFile(csvRecordsArray: any, headerLength: any) {
+    var dataArr = []
+
+    for (let i = 1; i < csvRecordsArray.length; i++) {
+      let data = csvRecordsArray[i].split(',');
+
+      // FOR EACH ROW IN CSV FILE IF THE NUMBER OF COLUMNS
+      // ARE SAME AS NUMBER OF HEADER COLUMNS THEN PARSE THE DATA
+      if (data.length == headerLength) {
+
+        var csvRecord: CSVRecord = new CSVRecord();
+
+        csvRecord.firstName = data[0].trim();
+        csvRecord.lastName = data[1].trim();
+        csvRecord.email = data[2].trim();
+        csvRecord.phoneNumber = data[3].trim();
+        csvRecord.title = data[4].trim();
+        csvRecord.occupation = data[5].trim();
+
+        dataArr.push(csvRecord);
+      }
+    }
+    return dataArr;
+  }
+
+  // CHECK IF FILE IS A VALID CSV FILE
+  isCSVFile(file: any) {
+    return file.name.endsWith(".csv");
+  }
+
+  // GET CSV FILE HEADER COLUMNS
+  getHeaderArray(csvRecordsArr: any) {
+    let headers = csvRecordsArr[0].split(',');
+    let headerArray = [];
+    for (let j = 0; j < headers.length; j++) {
+      headerArray.push(headers[j]);
+    }
+    return headerArray;
+  }
+
+  fileReset() {
+    this.fileImportInput.nativeElement.value = "";
+    this.csvRecords = [];
+  }
+
+}
+
+export class CSVRecord{
+
+  public firstName: any;
+  public lastName: any;
+  public email: any;
+  public phoneNumber: any;
+  public title: any;
+  public occupation: any;
+
+  constructor()
+  {
+
+  }
+}
